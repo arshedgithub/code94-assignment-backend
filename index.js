1cd0bb6ee536ab5d58b06a7e05fede5c74c88bca
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-04-28 06:42:44 +0530

    add routes

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 const express = require("express");
 const app = express();
-const UserModel = require("./models/user");
 const cors = require("cors");
+
+// connect to the database
 const connectDB = require("./config/db");
+
+// import route files
+const products = require("./routes/products");
+const users = require("./routes/users");
 
 app.use(express.json());
 app.use(cors());
 
 connectDB();
 
-app.get("/getUsers", (req, res) => {
-  UserModel.find({}, (err, result) => {
-    if (err) return res.json(err);
-    else return res.json(result);
-  });
-});
-
-app.post("/createUser", async (req, res) => {
-  const user = req.body;
-  const newUser = new UserModel(user);
-  await newUser.save();
-
-  res.json(user);
-});
+app.use("/api/products", products);
+app.use("/api/users", users);
 
 app.listen(4000, () => console.log("App is listening on port 4000...."));
